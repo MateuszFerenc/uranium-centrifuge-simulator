74b2a476302240ca9cd9b329ba6254af1bcfb5fd
MateuszFerenc <mateoferenc@gmail.com>
2022-08-25 01:42:09 +0200

    No new files added, only code changes. version_supervisor.py script is now working. Updated README.md

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ My recreation of old project, of uranium centrifuge simulator for school project
 # TODOs:
 * Add windows logic
 * Add windows contents
-* Add version supervisor script
 * ...
 
 # Done
 * Language support
 * Add language window update
+* Add version supervisor script
--- version_supervisor.py
+++ version_supervisor.py
@@ -1,33 +1,58 @@
 import sys
 from os import system, abort
-from re import match
 from datetime import datetime
 
 if __name__ == "__main__":
-    if match(".py$", sys.argv[1]):
+    if sys.argv[1].endswith(".py"):
         system(sys.argv[1])
     else:
         print("No python file specified!")
         abort()
 
-    usr_input = input("Does this version of project work? (if yes, automatically update project version")
+    usr_input = input("Does this version of project work? (if yes, update project version with specified type)")
     if usr_input.lower() in ("y", "yes", "yeah", "yep"):
         data = {}
-        file_exists = False
         try:
             with open("version_status", "r") as version_data:
-                file_exists = True
                 for line in version_data:
                     split_line = line.strip().split("#", 1)
                     data[split_line[0]] = split_line[1]
+                version_data.close()
         except FileNotFoundError:
             pass
-        finally:
-            version_data.close()
-        if file_exists:
-            pass
+        stable, beta, alfa = 0, 0, 0  # change all to 0, when you figure out how to display them
+        stable_max, beta_max, alfa_max = 9, 99, 999
+        if len(data) > 0:
+            usr_input = ''
+            while usr_input not in ('s', 'b', 'a'):
+                usr_input = input("Select release type (s - stable, b - beta, a - alfa) [abort for cancellation]")
+                if usr_input == 'abort':
+                    abort()
+            split_line = data['version'].strip().split(".")
+            stable, beta, alfa = int(split_line[0].replace('V', '')), int(split_line[1]), int(split_line[2])
+            if usr_input == 'a':
+                if alfa < alfa_max:
+                    alfa += 1
+                else:
+                    alfa = 0
+                    usr_input = 'b'
+            if usr_input == 'b':
+                if beta < beta_max:
+                    beta += 1
+                    alfa = 0
+                else:
+                    beta = 0
+                    usr_input = 's'
+            if usr_input == 's':
+                if stable < stable_max:
+                    stable += 1
+                    alfa, beta = 0, 0
+                else:
+                    print("Reached maximal version!")
+                    stable, beta, alfa = 0, 0, 0
         version_data = open("version_status", "w")
-        version_data.write("version#V0.00.000")
-        version_data.write("release_date#00/00/00".format(datetime.strftime("%d"),
-                                                          datetime.strftime("%m"), datetime.strftime("%Y")))
+        version_data.write(f"version#V{stable:1n}.{beta:02n}.{alfa:003n}\n")
+        now = datetime.now()
+        version_data.write("release_date#{}/{}/{}".format(now.strftime("%d"),
+                                                          now.strftime("%m"), now.strftime("%Y")))
         version_data.close()
